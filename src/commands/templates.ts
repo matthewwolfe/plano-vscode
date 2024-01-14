ee6436f62ee748f1483240826cebda9e3627e927
swisscodes <Lukas1569@gmail.com>
2024-01-14 18:03:48 -0500

    Add snippets contributions to extension + default snippet (useAuth hook) + some folder/file structure changes (#1)
    Co-authored-by: Lukas Hostettler

---
+++ src/commands/templates.ts
@@ -0,0 +1,81 @@
+import * as vscode from "vscode";
+import * as path from "node:path";
+import { getAllPaths, getTemplate } from "plano-cli";
+import { input } from "../input";
+import { selection } from "../selection";
+import { worker } from "../worker";
+
+export const templates = async (contextMenu: vscode.Uri) => {
+  let disposableStatus: vscode.Disposable | undefined;
+  try {
+    const templatePaths = getAllPaths([]);
+
+    const name = await selection(
+      templatePaths.map((templatePath) => ({
+        label: templatePath.template,
+        value: templatePath.template,
+      }))
+    );
+
+    const template = getTemplate({ name, paths: [] });
+
+    worker.on("message", async ({ type, ...args }) => {
+      switch (type) {
+        case "context-prompts": {
+          const { prompts } = args;
+
+          const contextValues = new Map<string, string>();
+
+          for (const prompt of prompts) {
+            if (prompt.type === "input") {
+              const answer = await input({
+                title: prompt.message,
+              });
+
+              contextValues.set(prompt.name, answer);
+            }
+          }
+
+          disposableStatus = vscode.window.setStatusBarMessage(
+            `Generating template "${template.template}"`
+          );
+
+          return worker.postMessage({
+            type: "generate",
+            data: {
+              copyToPath: contextMenu.path,
+              context: Object.fromEntries(contextValues),
+              modulePath: path.resolve(
+                `${template.path}/${template.template}`,
+                "context.mjs"
+              ),
+              template,
+            },
+          });
+        }
+
+        case "generate-success": {
+          if (disposableStatus) {
+            return disposableStatus.dispose();
+          }
+        }
+
+        default: {
+          vscode.window.showErrorMessage("Something went wrong");
+        }
+      }
+    });
+
+    worker.postMessage({
+      type: "import",
+      data: {
+        modulePath: path.resolve(
+          `${template.path}/${template.template}`,
+          "context.mjs"
+        ),
+      },
+    });
+  } catch (e) {
+    vscode.window.showErrorMessage(e as string);
+  }
+};
